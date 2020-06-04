e4f66a291d121a6333ef23d0a2146422d36d93fd
AaronLopes <aaronlopes@me.com>
2020-06-04 16:53:27 +0000

    change to for-of loop, quotations to match Google js style

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,11 +16,11 @@
  * Cycles through greetings in different languages for front page. 
  */
 const greetings =['Hi, nice to meet you!', '¡Hola, encantado de conocerte!', '你好，很高興見到你', 'Salut! Enchanté!'];
-var textElement = document.getElementById("changeText");
+var textElement = document.getElementById('changeText');
 textElement.innerHTML = greetings[0];
 var counter = 1;
 var intervalLength = setInterval(change, 2000);
-document.getElementById("messagesBtn").addEventListener("click", getMessages);
+document.getElementById('messagesBtn').addEventListener('click', getMessages);
 
 function change() {
   textElement.innerHTML = greetings[counter];
@@ -32,11 +32,10 @@ function change() {
 
 function getMessages() {
   fetch('/data').then(response => response.json()).then((messages) => {
-    const messageListElement = document.getElementById("messages-container");
+    const messageListElement = document.getElementById('messages-container');
     messageListElement.innerHTML = '';
-    for (let i = 0; i < messages.length; i++) {
-        messageListElement.appendChild(
-        createListElement(messages[i]));
+    for (let message of messages) {
+        messageListElement.appendChild(createListElement(message));
     }
   });
 }
