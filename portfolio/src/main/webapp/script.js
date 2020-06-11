1d1303130c41cd6bbb64c8dc4732c56c4fcba514
AaronLopes <aaronlopes@me.com>
2020-06-11 23:49:51 +0000

    create class for marker

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -22,30 +22,40 @@ let counter = 1;
 setInterval(change, 2000);
 document.getElementById('commentBtn').addEventListener('click', getComments);
 
+class Landmark {
+  constructor(title, lat, lng, description) {
+    this.title = title;
+    this.lat = lat;
+    this.lng = lng;
+    this.description = description;     
+  }
+}
+
 function initMap() {
     const map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 37.422403, lng: -122.088073}, zoom: 5});
-    const titles = ['Home', 'UMass Amherst', 'Georgia Tech', 'Home Away From Home', 'An Extraordinary Layover'];
-    const lats = [37.3230, 42.3868, 33.7756, 19.4564, 51.5074];
-    const lngs = [-122.0322,-72.5301, -84.3963, 72.7925, -0.1278]
-    const descriptions = ['Where I currently live, and spent my high school years. Home to Apple HQ, the best boba, \
-    and my closest friends :)', 'Snowy, remote, but cozy; where I spent my first 1.5 years of college.', 'Where I \
-    currently attend college!', 'Where my parents grew up; the comfort of my extended family and the cities unique \
-    culture always brings me back', 'Spent an ambitious 8 hours searching for all the famous landmarks, check out pictures in misc!'];
-    for (let i = 0; i < titles.length; i++) {
-        addLandmark(map, lats[i], lngs[i], titles[i], descriptions[i])
+    
+    let landmarks = [new Landmark('Home', 37.3230, -122.0322, 'Where I currently live, and spent my high school years. \
+    Home to Apple HQ, the best boba, and my closest friends :)'), new Landmark('UMass Amherst', 42.3868, -72.5301, 'Snowy, \
+    remote, but cozy; where I spent my first 1.5 years of college.'), new Landmark('Georgia Tech', 33.7756, -84.3963, 'Where I \
+    currently attend college!'), new Landmark('Home, Away From Home', 19.4564, 72.7925, 'Where my parents grew up; the comfort \
+    of my extended family and the cities unique culture always brings me back'), new Landmark('An Extraordinary Layover',
+    51.5074, -0.1278, 'Spent an ambitious 8 hours searching for all the famous landmarks, check out pictures in misc!') ]
+
+    for (let landmark of landmarks) {
+        addLandmark(map, landmark)
     }
 };
 
-function addLandmark(map, lat, lng, title, description) {
+function addLandmark(map, Landmark) {
   const marker = new google.maps.Marker(
-      {position: {lat: lat, lng: lng}, map: map, title: title});
+      {position: {lat: Landmark.lat, lng: Landmark.lng}, map: map, title: Landmark.title});
+  const infoWindow = new google.maps.InfoWindow({content: Landmark.description});
 
-  const infoWindow = new google.maps.InfoWindow({content: description});
   marker.addListener('click', () => {
     infoWindow.open(map, marker);
-    let latLng = new google.maps.LatLng(lat, lng);
+    let latLng = new google.maps.LatLng(Landmark.lat, Landmark.lng);
     map.panTo(latLng);
     map.setZoom(10);
   });
