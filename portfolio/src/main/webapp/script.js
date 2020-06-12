a317ab2838f65b38521a63dfd4e366d45852434f
AaronLopes <aaronlopes@me.com>
2020-06-12 17:29:01 +0000

    add function to close other infowindows onclick, clean formatting

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,15 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Cycles through greetings in different languages for front page. 
- */
 const greetings =['Hi, nice to meet you!', '¡Hola, encantado de conocerte!', '你好，很高興見到你', 'Salut! Enchanté!'];
-let textElement = document.getElementById('changeText');
+const textElement = document.getElementById('changeText');
 textElement.innerHTML = greetings[0];
 let counter = 1;
 setInterval(change, 2000);
 document.getElementById('commentBtn').addEventListener('click', getComments);
+const markers = []
+
+/**
+ * Cycles through greetings in different languages for front page. 
+ */
+function change() {
+  textElement.innerHTML = greetings[counter];
+  counter++;
+  if (counter >= greetings.length) {
+    counter = 0;
+  }
+}
 
 class Landmark {
   constructor(title, lat, lng, description) {
@@ -34,39 +43,56 @@ class Landmark {
 function initMap() {
     const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422403, lng: -122.088073}, zoom: 5});
-    
-    let landmarks = [new Landmark('Home', 37.3230, -122.0322, 'Where I currently live, and spent my high school years. \
-    Home to Apple HQ, the best boba, and my closest friends :)'), new Landmark('UMass Amherst', 42.3868, -72.5301, 'Snowy, \
-    remote, but cozy; where I spent my first 1.5 years of college.'), new Landmark('Georgia Tech', 33.7756, -84.3963, 'Where I \
-    currently attend college!'), new Landmark('Home, Away From Home', 19.4564, 72.7925, 'Where my parents grew up; the comfort \
-    of my extended family and the cities unique culture always brings me back'), new Landmark('An Extraordinary Layover',
-    51.5074, -0.1278, 'Spent an ambitious 8 hours searching for all the famous landmarks, check out pictures in misc!') ]
+      {
+        center: {
+          lat: 37.422403,
+          lng: -122.088073
+        },
+        zoom: 5
+      }
+    );
+
+    const landmarks = [
+      new Landmark('Home', 37.3230, -122.0322, 'Where I currently live, and spent my high school years. \
+        Home to Apple HQ, the best boba, and my closest friends :)'),
+      new Landmark('UMass Amherst', 42.3868, -72.5301, 'Snowy, remote, but cozy; where I spent my first \
+        1.5 years of college.'),
+      new Landmark('Georgia Tech', 33.7756, -84.3963, 'Where I currently attend college!'),
+      new Landmark('Home, Away From Home', 19.4564, 72.7925, 'Where my parents grew up; the comfort of my \
+        extended family and the cities unique culture always brings me back'),
+      new Landmark('An Extraordinary Layover', 51.5074, -0.1278, 'Spent an ambitious 8 hours searching for \
+        all the famous landmarks, check out pictures in misc!')
+    ];
 
     for (let landmark of landmarks) {
-        addLandmark(map, landmark)
+      addLandmark(map, landmark);
     }
 };
 
-function addLandmark(map, Landmark) {
+function addLandmark(map, landmark) {
+  const infoWindow = new google.maps.InfoWindow({content: landmark.description});
   const marker = new google.maps.Marker(
-      {position: {lat: Landmark.lat, lng: Landmark.lng}, map: map, title: Landmark.title});
-  const infoWindow = new google.maps.InfoWindow({content: Landmark.description});
-
+    {
+      position: {lat: landmark.lat, lng: landmark.lng},
+      map: map,
+      title: landmark.title,
+      info: infoWindow
+    }
+  );
+  markers.push(marker);
   marker.addListener('click', () => {
-    infoWindow.open(map, marker);
-    let latLng = new google.maps.LatLng(Landmark.lat, Landmark.lng);
+    closeAllInfoWindows(map);
+    marker.info.open(map, marker);
+    let latLng = new google.maps.LatLng(landmark.lat, landmark.lng);
     map.panTo(latLng);
     map.setZoom(10);
   });
 }
 
-function change() {
-  textElement.innerHTML = greetings[counter];
-  counter++;
-  if (counter >= greetings.length) {
-    counter = 0;
-  }
+function closeAllInfoWindows(map) {
+  markers.forEach(function(marker) {
+    marker.info.close(map, marker);
+  });
 }
 
 function getComments() {
