83435489e68072314696c2ad45b83901cbd12c32
AaronLopes <aaronlopes@me.com>
2020-06-18 17:47:09 -0700

    rough query() solution

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -16,18 +16,91 @@ package com.google.sps;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     // MEETING_REQUEST has name, duration in minutes, collection of attendees
     // event in EVENTS has name, time range, collection of attendees
 
-    Collection<TimeRange> ranges = new ArrayList<TimeRange>();
-    if (request.getAttendees().isEmpty()) {
-        ranges.add(TimeRange.WHOLE_DAY);
-        return ranges;
+    List<Event> overlapEvents = new ArrayList<Event>();
+    List<TimeRange> overlapRanges = new ArrayList<TimeRange>();
+    List<TimeRange> noOverlapRanges = new ArrayList<TimeRange>();
+    Collection<String> requestAttendees = request.getAttendees();
+    List<TimeRange> possibleRanges = new ArrayList<TimeRange>();
+    int requestDuration = (int) request.getDuration();
+
+    if (requestDuration > TimeRange.WHOLE_DAY.duration()) {
+      return possibleRanges;
     }
-    
-    return ranges;
+   
+    // inefficient, optimize later
+    for (String attendee : requestAttendees) {
+      for (Event event : events) {
+        if (event.getAttendees().contains(attendee)) {
+          overlapEvents.add(event);
+        }
+      }
+    }
+
+    if (overlapEvents.isEmpty()) {
+      possibleRanges.add(TimeRange.WHOLE_DAY);
+      return possibleRanges;
+    }
+
+    // merge overlapping ranges into one range    
+    for (Event event : overlapEvents) {
+      overlapRanges.add(event.getWhen());
+    }
+
+    Collections.sort(overlapRanges, TimeRange.ORDER_BY_START);
+  
+    int i = 0;
+    while (i < overlapRanges.size()) {
+      TimeRange currRange = overlapRanges.get(i);
+      int j = i + 1;
+      while (j < overlapRanges.size()) {
+        TimeRange nextRange = overlapRanges.get(j);
+        if (currRange.overlaps(nextRange)) {
+          int modEnd = currRange.end() > nextRange.end() ? currRange.end() : nextRange.end();
+          currRange = TimeRange.fromStartEnd(currRange.start(), modEnd, false);
+          i = j;
+          j++;
+        } else {
+          break;
+        }
+      }
+      noOverlapRanges.add(currRange);
+      i++;
+    }
+
+    if (noOverlapRanges.get(0).start() > TimeRange.START_OF_DAY) {
+      possibleRanges.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, noOverlapRanges.get(0).start(), false));
+    }
+
+    i = 0;
+    while (i < noOverlapRanges.size()) {
+      TimeRange currRange = noOverlapRanges.get(i);
+      int j = i + 1;
+      if (j >= noOverlapRanges.size()) {
+        if (currRange.end() < TimeRange.END_OF_DAY) {
+          possibleRanges.add(TimeRange.fromStartEnd(currRange.end(), TimeRange.END_OF_DAY, true));
+        }
+      } else {
+        possibleRanges.add(TimeRange.fromStartEnd(currRange.end(), noOverlapRanges.get(j).start(), false));
+      }
+      i++;
+    }
+
+    // remove ranges which are shorter than 
+    for (i = 0; i < possibleRanges.size(); i++) {
+      if (possibleRanges.get(i).duration() < requestDuration) {
+        possibleRanges.remove(i);
+      }
+    }
+
+    return possibleRanges;
   }
+
 }
